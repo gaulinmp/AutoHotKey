0355cb5b1a20ca679174abb3a378803316ca8b11
Mac Gaulin <gaulinmp@gmail.com>
2017-07-31 14:09:11 -0600

    Add startup batch script

---
+++ start.bat
@@ -0,0 +1,5 @@
+# AutoHotkey must be on the path. If not, replace the exe below with full path.
+
+start "" "AutoHotkeyU64.exe" "%~dp0\alt-drag-window-resize.ahk"
+
+start "" "AutoHotkeyU64.exe" "%~dp0\desktop-scroll-switch.ahk"
